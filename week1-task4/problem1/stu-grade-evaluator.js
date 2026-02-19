c1a6d2624a6be8ea0d2b5001cb7782a84363c821
shreya737-ai <shreyabanerjee737@gmail.com>
2026-02-19 15:30:38 +0530

    Added week1-task4

---
+++ week1-task4/problem1/stu-grade-evaluator.js
@@ -0,0 +1,13 @@
+let a=67
+if (a>= 75){
+    console.log("Grade A")
+}
+else if (a>= 60){
+    console.log("Grade B")
+}
+else if (a>= 40){
+    console.log("Grade C")
+}   
+else {
+    console.log("Fail")
+}
---
+++ week1-task4/problem3/trafic-signal-simulator.js
@@ -0,0 +1,16 @@
+let a='red'
+let b='yellow'
+let c='green'
+switch (b) {
+    case a:
+        console.log("Stop")
+        break;
+    case b:
+        console.log("Get ready")
+        break;
+    case c:
+        console.log("Go")
+        break;
+    default:
+        console.log("Invalid Signal")
+}
